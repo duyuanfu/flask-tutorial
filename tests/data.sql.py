61cd128301282b688b3d7abc57497817451fd8e2
dyf <duyuanfu@yeah.net>
2020-11-09 20:26:31 +0800

    第一次提交

---
+++ tests/data.sql.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/11/8 下午9:57
+# @Author  : yuanfu.du
+# @Site    : 
+# @File    : data.sql.py
---
+++ tests/test_db.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/11/8 下午9:58
+# @Author  : yuanfu.du
+# @Site    : 
+# @File    : test_db.py
